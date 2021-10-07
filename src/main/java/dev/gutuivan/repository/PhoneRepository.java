56c26c218c52faab8b9076f3e7ffe2cd69d78790
Gutu Ivan <gutsu.ivan@mail.ru>
2021-10-07 10:37:07 +0300

    Created: +Phone repository

---
+++ src/main/java/dev/gutuivan/repository/PhoneRepository.java
@@ -0,0 +1,9 @@
+package dev.gutuivan.repository;
+
+import dev.gutuivan.model.Phone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PhoneRepository extends JpaRepository<Phone, Long> {
+}
