3010ec96843252490e3f17e0af85ca2a57a6d454
Gutu Ivan <gutsu.ivan@mail.ru>
2021-10-07 09:10:34 +0300

    Initial commit

---
+++ src/main/java/dev/gutuivan/Task071021Application.java
@@ -0,0 +1,13 @@
+package dev.gutuivan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task071021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task071021Application.class, args);
+	}
+
+}
