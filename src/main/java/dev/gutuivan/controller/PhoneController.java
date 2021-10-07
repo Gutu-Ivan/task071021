0bc6ca603e152b22b42e4aacffbf9d8ff0f9f2c6
Gutu Ivan <gutsu.ivan@mail.ru>
2021-10-07 10:13:29 +0300

    Created: +Phone controller

---
+++ src/main/java/dev/gutuivan/controller/PhoneController.java
@@ -0,0 +1,12 @@
+package dev.gutuivan.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/phones/")
+public class PhoneController {
+    public static void main(String[] args) {
+
+    }
+}
